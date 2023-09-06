84e5b7bdf3a381ec4f8f6ab14aada3ee2fd7f59e
antaww <anta.pizzetta@gmail.com>
2023-09-06 14:51:42 +0200

    feat(gui): Screen positions get with pyautogui.

---
+++ main.py
@@ -0,0 +1,16 @@
+from mss import mss  # Screen capture
+import cv2  # Image processing
+import time  # Time
+import serial  # Serial communication
+import pyautogui  # Mouse and keyboard control
+
+# while True:
+#     print(pyautogui.position())  # Print mouse position
+#     time.sleep(0.3)
+
+
+# screen positions
+# top left : 614, 60
+# top right : 830, 60
+# bottom right : 830, 223
+# bottom left : 614, 223
